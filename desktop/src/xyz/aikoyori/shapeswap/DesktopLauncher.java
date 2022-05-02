a595e63fd9d944dcb8b9affdf4768147e51ae33d
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2022-05-03 02:01:08 +0700

    ICON

--- desktop/src/xyz/aikoyori/shapeswap/DesktopLauncher.java
+++ desktop/src/xyz/aikoyori/shapeswap/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package xyz.aikoyori.shapeswap;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import xyz.aikoyori.shapeswap.ShapeSwap;
@@ -10,6 +11,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setWindowSizeLimits(256,144,99999,99999);
+		config.setWindowIcon( Files.FileType.Internal,"assets/textures/shapeswap.png");
 		config.setWindowedMode(1280,720);
 		config.setBackBufferConfig(8, 8, 8,8, 1,0, 16);
 		new Lwjgl3Application(new ShapeSwap(), config);
